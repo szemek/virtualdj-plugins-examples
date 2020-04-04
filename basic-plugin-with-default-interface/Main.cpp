16d04c5cccb706feffbd91b5e23b7af6052b1c91
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2020-04-04 11:04:55 +0200

    Initial commit

---
+++ basic-plugin-with-default-interface/Main.cpp
@@ -0,0 +1,17 @@
+#include "MyPlugin8.h"
+
+// This is the standard DLL loader for COM object.
+
+HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppObject)
+{
+    if (memcmp(&rclsid,&CLSID_VdjPlugin8,sizeof(GUID))==0 && memcmp(&riid,&IID_IVdjPluginBasic8,sizeof(GUID))==0)
+    {
+        *ppObject=new CMyPlugin8();
+    }
+    else
+    {
+        return CLASS_E_CLASSNOTAVAILABLE;
+    }
+
+    return NO_ERROR;
+}
